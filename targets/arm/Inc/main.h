ffe987ebb3eab591cb50aa585a900f910dd322d8
witek117 <witek117@gmail.com>
2019-10-07 19:51:35 +0200

    CPU config from CubeMX

--- targets/arm/Inc/main.h
+++ targets/arm/Inc/main.h
@@ -58,6 +58,76 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define PG_3V3_Pin GPIO_PIN_13
+#define PG_3V3_GPIO_Port GPIOC
+#define SENS_IR_LED_Pin GPIO_PIN_14
+#define SENS_IR_LED_GPIO_Port GPIOC
+#define TRIG_Pin GPIO_PIN_15
+#define TRIG_GPIO_Port GPIOC
+#define ECHO_Pin GPIO_PIN_0
+#define ECHO_GPIO_Port GPIOC
+#define SENS8_Pin GPIO_PIN_1
+#define SENS8_GPIO_Port GPIOC
+#define SENS7_Pin GPIO_PIN_2
+#define SENS7_GPIO_Port GPIOC
+#define SENS6_Pin GPIO_PIN_3
+#define SENS6_GPIO_Port GPIOC
+#define SENS5_Pin GPIO_PIN_0
+#define SENS5_GPIO_Port GPIOA
+#define SENS4_Pin GPIO_PIN_1
+#define SENS4_GPIO_Port GPIOA
+#define RPI_TXD_Pin GPIO_PIN_2
+#define RPI_TXD_GPIO_Port GPIOA
+#define RPI_RXD_Pin GPIO_PIN_3
+#define RPI_RXD_GPIO_Port GPIOA
+#define SENS3_Pin GPIO_PIN_4
+#define SENS3_GPIO_Port GPIOA
+#define SENS2_Pin GPIO_PIN_5
+#define SENS2_GPIO_Port GPIOA
+#define SENS1_Pin GPIO_PIN_6
+#define SENS1_GPIO_Port GPIOA
+#define TEMP_Pin GPIO_PIN_7
+#define TEMP_GPIO_Port GPIOA
+#define WS2812B_Pin GPIO_PIN_4
+#define WS2812B_GPIO_Port GPIOC
+#define LED1_Pin GPIO_PIN_5
+#define LED1_GPIO_Port GPIOC
+#define V_CURRENT_SENS_Pin GPIO_PIN_0
+#define V_CURRENT_SENS_GPIO_Port GPIOB
+#define V_BAT_Pin GPIO_PIN_1
+#define V_BAT_GPIO_Port GPIOB
+#define V_5V_Pin GPIO_PIN_2
+#define V_5V_GPIO_Port GPIOB
+#define SHDN_MOTOR_TPS_Pin GPIO_PIN_12
+#define SHDN_MOTOR_TPS_GPIO_Port GPIOB
+#define DIP_SW_Pin GPIO_PIN_6
+#define DIP_SW_GPIO_Port GPIOC
+#define PG_5V_Pin GPIO_PIN_7
+#define PG_5V_GPIO_Port GPIOC
+#define INT_CHARGER_Pin GPIO_PIN_8
+#define INT_CHARGER_GPIO_Port GPIOC
+#define PG_3V3_ESP_Pin GPIO_PIN_9
+#define PG_3V3_ESP_GPIO_Port GPIOC
+#define EN_PWR_ESP_Pin GPIO_PIN_8
+#define EN_PWR_ESP_GPIO_Port GPIOA
+#define USART1_TXD_Pin GPIO_PIN_9
+#define USART1_TXD_GPIO_Port GPIOA
+#define USART1_RXD_Pin GPIO_PIN_10
+#define USART1_RXD_GPIO_Port GPIOA
+#define LED2_Pin GPIO_PIN_11
+#define LED2_GPIO_Port GPIOA
+#define PG_6V_Pin GPIO_PIN_12
+#define PG_6V_GPIO_Port GPIOA
+#define SW1_Pin GPIO_PIN_15
+#define SW1_GPIO_Port GPIOA
+#define ESP_TXD_Pin GPIO_PIN_10
+#define ESP_TXD_GPIO_Port GPIOC
+#define ESP_RXD_Pin GPIO_PIN_11
+#define ESP_RXD_GPIO_Port GPIOC
+#define INT_BHI_Pin GPIO_PIN_12
+#define INT_BHI_GPIO_Port GPIOC
+#define INT_MPU_6050_Pin GPIO_PIN_3
+#define INT_MPU_6050_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- targets/arm/Src/main.c
+++ targets/arm/Src/main.c
@@ -42,6 +42,13 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
+I2C_HandleTypeDef hi2c1;
+
+UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart3;
 
 /* USER CODE BEGIN PV */
 
@@ -49,6 +56,12 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_USART3_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -85,6 +98,12 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
+  MX_I2C1_Init();
+  MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -108,6 +127,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the CPU, AHB and APB busses clocks 
   */
@@ -132,6 +152,283 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+  /** Common config 
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Regular Channel 
+  */
+  sConfig.Channel = ADC_CHANNEL_7;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 115200;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, SENS_IR_LED_Pin|TRIG_Pin|LED1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(SHDN_MOTOR_TPS_GPIO_Port, SHDN_MOTOR_TPS_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PG_3V3_Pin SENS8_Pin SENS7_Pin SENS6_Pin 
+                           DIP_SW_Pin PG_5V_Pin PG_3V3_ESP_Pin */
+  GPIO_InitStruct.Pin = PG_3V3_Pin|SENS8_Pin|SENS7_Pin|SENS6_Pin 
+                          |DIP_SW_Pin|PG_5V_Pin|PG_3V3_ESP_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SENS_IR_LED_Pin TRIG_Pin LED1_Pin */
+  GPIO_InitStruct.Pin = SENS_IR_LED_Pin|TRIG_Pin|LED1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : ECHO_Pin WS2812B_Pin INT_CHARGER_Pin INT_BHI_Pin */
+  GPIO_InitStruct.Pin = ECHO_Pin|WS2812B_Pin|INT_CHARGER_Pin|INT_BHI_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SENS5_Pin SENS4_Pin SENS3_Pin SENS2_Pin 
+                           SENS1_Pin PG_6V_Pin */
+  GPIO_InitStruct.Pin = SENS5_Pin|SENS4_Pin|SENS3_Pin|SENS2_Pin 
+                          |SENS1_Pin|PG_6V_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : V_5V_Pin */
+  GPIO_InitStruct.Pin = V_5V_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(V_5V_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SHDN_MOTOR_TPS_Pin */
+  GPIO_InitStruct.Pin = SHDN_MOTOR_TPS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(SHDN_MOTOR_TPS_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : EN_PWR_ESP_Pin */
+  GPIO_InitStruct.Pin = EN_PWR_ESP_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(EN_PWR_ESP_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LED2_Pin */
+  GPIO_InitStruct.Pin = LED2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SW1_Pin */
+  GPIO_InitStruct.Pin = SW1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(SW1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : INT_MPU_6050_Pin */
+  GPIO_InitStruct.Pin = INT_MPU_6050_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(INT_MPU_6050_GPIO_Port, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
