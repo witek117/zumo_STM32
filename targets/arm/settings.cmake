c63e8584eea52733a3161b2d381e030427ed8cb6
witek117 <gednotxa::99>
2020-05-15 02:17:42 +0200

    cmake repair

--- targets/arm/settings.cmake
+++ targets/arm/settings.cmake
@@ -1,22 +1,19 @@
-set (CWARN "-Wall -Wstrict-prototypes -Wextra ") #-Werror
-set (CXXWARN "-Wall -Wextra ") #-Werror
-set(CTUNING "-fomit-frame-pointer -ffunction-sections -fdata-sections")
+set(CWARN               "-Wall -Wextra ") #-Wstrict-prototypes -Werror
+set(CXXWARN             "-Wall -Wextra ") #-Werror
+set(CTUNING             "-fomit-frame-pointer -ffunction-sections -fdata-sections")
 
-ReadVariables(${CMAKE_CURRENT_LIST_DIR}/Makefile "CPU")        # get flag -DCPU = -mcpu=cortex-mXX
+ReadVariables(${CMAKE_CURRENT_LIST_DIR}/Makefile "CPU")             # get flag -DCPU = -mcpu=cortex-mXX
 ReadVariables(${CMAKE_CURRENT_LIST_DIR}/Makefile "FPU")
 ReadVariables(${CMAKE_CURRENT_LIST_DIR}/Makefile "FLOAT-ABI")
-ReadVariables(${CMAKE_CURRENT_LIST_DIR}/Makefile "C_DEFS")        # get flag -DSTM32F***
+ReadVariables(${CMAKE_CURRENT_LIST_DIR}/Makefile "C_DEFS")          # get flag -DSTM32F***
 
-message(STATUS "${CPU} ${FPU} ${FLOAT-ABI} ${C_DEFS}")
+set(ARMFLOAT            "-mfpu=${FPU} -mfloat-abi=${FLOAT-ABI}")
+set(CMCU                "-mcpu=${CPU} -mthumb ${ARMFLOAT}")
 
-set (ARMFLOAT "-mfpu=${FPU} -mfloat-abi=${FLOAT-ABI}")
-set(CMCU "-mcpu=${CPU} -mthumb ${ARMFLOAT}")
+string(REPLACE          ";" " " C_DEFS "${C_DEFS}")
 
-string(REPLACE ";" " " C_DEFS "${C_DEFS}")
-message("C_DEFS: ${C_DEFS}")
-
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu11 ${CWARN} ${CTUNING} ${CMCU} ${C_DEFS}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++17 -Dregister=\"/**/\" -fno-exceptions -fno-rtti ${CXXWARN} ${CTUNING} ${CMCU} ${C_DEFS}")
+set(CMAKE_C_FLAGS       "${CMAKE_C_FLAGS} -std=gnu11 ${CWARN} ${CTUNING} ${CMCU} ${C_DEFS}")
+set(CMAKE_CXX_FLAGS     "${CMAKE_CXX_FLAGS} -std=gnu++17 -fno-exceptions -fno-rtti ${CXXWARN} ${CTUNING} ${CMCU} ${C_DEFS}")
 
 ReadVariables(${CMAKE_CURRENT_LIST_DIR}/Makefile "LDSCRIPT")      # get LD file
 set(PLATFORM_LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/${LDSCRIPT})
